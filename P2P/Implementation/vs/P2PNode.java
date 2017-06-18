102683abbe3cac18e1ed7b560ee7f5d7905d5989
Patri <Patri@T470Patrick.fritz.box>
2017-06-18 14:31:55 +0200

    Class P2PNode erzeugt

---
+++ P2P/Implementation/vs/P2PNode.java
@@ -0,0 +1,61 @@
+package vs;
+
+import java.util.Iterator;
+
+public class P2PNode implements IP2PNode {
+
+	@Override
+	public IP2PNode findSuccessor(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IP2PNode getPredecessor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IP2PNode getSuccessor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void notify(IP2PNode newPredecessor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<IP2PNode> getFingers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean store(String key, String value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String lookup(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printStoredData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
